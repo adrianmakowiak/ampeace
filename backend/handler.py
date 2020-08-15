313175bb6c3f8615558247b083073dd7e16883cd
adrianmakowiak <adrian.makowiak@gmail.com>
2020-08-16 00:16:42 +0200

    feat(be): add lambda to download audio from s3

--- backend/handler.py
+++ backend/handler.py
@@ -1,16 +1,26 @@
 import json
+import base64
+import boto3
 from wrapper import lambda_wrapper
+
+s3 = boto3.client('s3')
 
 @lambda_wrapper
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    print(boto3)
+
+    response = s3.get_object(Bucket='ampeace-sounds', Key='mdi-fire.mp3')
+    print(response)
+
+    audio = response['Body'].read()
 
     response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
+        'statusCode': 200,
+        'body': base64.b64encode(audio),
+        'headers': {
+            "Content-Type": "audio/mpeg"
+        },
+        'isBase64Encoded': True
     }
 
     return response
