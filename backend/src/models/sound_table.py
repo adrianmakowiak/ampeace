7d0d5d6ff64a5789baeb16327f983f41b0a0c70f
adrianmakowiak <adrian.makowiak@gmail.com>
2020-08-16 23:49:33 +0200

    chore(be): add table model

---
+++ backend/src/models/sound_table.py
@@ -0,0 +1,12 @@
+from pynamodb.models import Model
+from pynamodb.attributes import (
+    UnicodeAttribute, NumberAttribute, UnicodeSetAttribute, UTCDateTimeAttribute
+)
+class SoundTable(Model):
+  class Meta:
+    table_name = 'dev-ampeace-sound-table'
+    region = 'eu-central-1'
+  PK = UnicodeAttribute(hash_key=True)
+  SK = UnicodeAttribute(range_key=True)
+  sound_type = UnicodeAttribute(attr_name='sT')
+  
