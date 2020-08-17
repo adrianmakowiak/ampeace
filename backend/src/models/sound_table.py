ced6f0a93fa977d8341efdb3014204861cd67c91
adrianmakowiak <adrian.makowiak@gmail.com>
2020-08-17 22:05:01 +0200

    chore(be): add __iter__ method to SoundTable

--- backend/src/models/sound_table.py
+++ backend/src/models/sound_table.py
@@ -6,8 +6,12 @@ class SoundTable(Model):
   class Meta:
     table_name = 'dev-ampeace-sound-table'
     region = 'eu-central-1'
-  PK = UnicodeAttribute(hash_key=True)
-  SK = UnicodeAttribute(range_key=True)
+  PK = UnicodeAttribute(hash_key=True, null=False)
+  SK = UnicodeAttribute(range_key=True, null=False)
   sound_type = UnicodeAttribute(attr_name='sT')
   sound_name = UnicodeAttribute(attr_name='sN')
+  
+  def __iter__(self):
+    for name, attr in self.get_attributes().items():
+      yield name, attr.serialize(getattr(self, name))
   
