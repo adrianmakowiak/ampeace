f82f1e4a1c01d4dad5bf60ff09294b7c8cb43412
adrianmakowiak <adrian.makowiak@gmail.com>
2020-08-17 19:37:07 +0200

    chore(be): update lambda iam role

--- backend/src/lambdas/sound.py
+++ backend/src/lambdas/sound.py
@@ -7,14 +7,15 @@ from src.wrapper.wrapper import lambda_wrapper
 from src.models.sound_table import SoundTable
 #pylint: enable=import-error
 
+BUCKET_NAME = 'ampeace-sounds'
+
 s3 = boto3.client('s3')
 dynamo = boto3.client('dynamodb')
 
 @lambda_wrapper
 def get(event, context):
-    print(boto3)
-    response = s3.get_object(Bucket='ampeace-sounds', Key='mdi-fire.mp3')
-    print(response)
+    sound_key = event['pathParameters']['id'] + '.mp3'
+    response = s3.get_object(Bucket=BUCKET_NAME, Key=sound_key)
 
     audio = response['Body'].read()
 
@@ -31,23 +32,24 @@ def get(event, context):
 
 @lambda_wrapper
 def post(event, context):
+    sound_name = event['queryStringParameters']['name']
     sound_key = str(uuid4())
     file_content = base64.b64decode(event['body'])
     s3_response = s3.put_object(
         Body=file_content, 
-        Bucket='ampeace-sounds', 
+        Bucket=BUCKET_NAME,
         Key=sound_key+'.mp3', ContentType='audio/mp3'
         )
-
     print(s3_response)
 
-    sound_item = SoundTable(PK='sound', SK=sound_key, sound_type='mp3')
+    sound_item = SoundTable(PK='sound', SK=sound_key, sound_type='mp3', sound_name=sound_name)
     sound_item.save()
 
     response = {
         'statusCode': 200,
         'body': json.dumps({
-            'soundId': sound_key
+            'soundId': sound_key,
+            'soundName': sound_name,
         })
     }
 
